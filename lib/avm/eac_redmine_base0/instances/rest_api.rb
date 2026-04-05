b361d839843dc0a8616c4c7e36e8ffebdf2a9d6c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-04-05 00:57:27 -0300

    Avm::EacRedmineBase0::Instances::RestApi.build_service_url.

--- lib/avm/eac_redmine_base0/instances/rest_api.rb
+++ lib/avm/eac_redmine_base0/instances/rest_api.rb
@@ -4,6 +4,12 @@ module Avm
   module EacRedmineBase0
     module Instances
       class RestApi < ::EacRest::Api
+        # @return [Addressable::URI]
+        def build_service_url(suffix)
+          r = super
+          r.path = "#{r.path}.json"
+          r
+        end
       end
     end
   end
